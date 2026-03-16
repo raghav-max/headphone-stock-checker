b996e4df4a2a0539d5f48b314fd781e878d4eda0
Raghav Mehrotra <meraghav@amazon.com>
2026-03-16 19:52:16 +0530

    Added stock checker script.

---
+++ check_stock.py
@@ -0,0 +1,23 @@
+import requests
+
+URL = "https://www.headphonezone.in/products/hifiman-arya-stealth-magnet-version.json"
+
+def check_stock():
+    r = requests.get(URL)
+    r.raise_for_status()
+
+    data = r.json()
+    variants = data["product"]["variants"]
+
+    for v in variants:
+        qty = v["inventory_quantity"]
+
+        if qty > 0:
+            print("🚨 IN STOCK")
+            print("https://www.headphonezone.in/products/hifiman-arya-stealth-magnet-version")
+            return
+
+    print("❌ Still sold out")
+
+if __name__ == "__main__":
+    check_stock()
