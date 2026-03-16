850cb4fb1c2784c27c0fadc2bd6ad464e6f23412
Raghav Mehrotra <meraghav@amazon.com>
2026-03-16 21:24:58 +0530

    Added ability to page my phone.

--- check_stock.py
+++ check_stock.py
@@ -1,6 +1,22 @@
 import requests
+import os
 
 URL = "https://www.headphonezone.in/products/hifiman-arya-stealth-magnet-version.json"
+
+USER_KEY = os.getenv("PUSHOVER_USER_KEY")
+API_TOKEN = os.getenv("PUSHOVER_API_TOKEN")
+
+def notify(msg):
+    requests.post(
+        "https://api.pushover.net/1/messages.json",
+        data={
+            "token": API_TOKEN,
+            "user": USER_KEY,
+            "message": msg,
+            "title": "Headphone Restock Alert",
+            "priority": 1
+        }
+    )
 
 def check_stock():
     r = requests.get(URL)
@@ -14,6 +30,10 @@ def check_stock():
         qty = v["inventory_quantity"]
 
         if qty > 0:
+            notify(
+                "🚨 Arya Stealth is in stock!\n"
+                "https://www.headphonezone.in/products/hifiman-arya-stealth-magnet-version"
+            )
             print("🚨 IN STOCK")
             print("https://www.headphonezone.in/products/hifiman-arya-stealth-magnet-version")
             return
