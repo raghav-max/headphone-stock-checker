52f61b60272aa0c3d48e4200d364f004d545f1d9
Raghav Mehrotra <meraghav@amazon.com>
2026-03-17 21:01:03 +0530

    Adding ability to check stock for dac.

--- check_stock.py
+++ check_stock.py
@@ -2,6 +2,7 @@ import requests
 import os
 
 URL = "https://www.headphonezone.in/products/hifiman-arya-stealth-magnet-version.json"
+URL2 = "https://www.headphonezone.in/products/topping-dx5-ii.json"
 
 USER_KEY = os.getenv("PUSHOVER_USER_KEY")
 API_TOKEN = os.getenv("PUSHOVER_API_TOKEN")
@@ -21,7 +22,29 @@ def notify(msg):
         }
     )
 
-def check_stock():
+def check_stock_dac():
+    r = requests.get(URL2)
+    r.raise_for_status()
+
+    data = r.json()
+    variants = data["product"]["variants"]
+
+    for v in variants:
+        print(v)
+        qty = v["inventory_quantity"]
+
+        if qty > 0:
+            notify(
+                "🚨 Topping DX5 II is in stock!\n"
+                "https://www.headphonezone.in/products/topping-dx5-ii"
+            )
+            print("🚨 IN STOCK")
+            print("https://www.headphonezone.in/products/topping-dx5-ii")
+            return
+
+    print("❌ Still sold out")
+
+def check_stock_headphone():
     r = requests.get(URL)
     r.raise_for_status()
 
@@ -44,4 +67,5 @@ def check_stock():
     print("❌ Still sold out")
 
 if __name__ == "__main__":
-    check_stock()
+    check_stock_headphone()
+    check_stock_dac()
